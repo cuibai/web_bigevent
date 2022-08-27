def77a4d1069fe871d88c16271c0a4eb4dbcccb1
cuibai <2367736060@qq.com>
2022-08-27 16:24:11 +0800

    0827学习完毕

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,3 +1,153 @@
+/*
+ * @Author: cuibai 2367736060@qq.com
+ * @Date: 2022-08-24 22:40:03
+ * @LastEditors: cuibai 2367736060@qq.com
+ * @LastEditTime: 2022-08-27 16:15:18
+ * @FilePath: \d01\assets\js\article\art_list.js
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by cuibai 2367736060@qq.com, All Rights Reserved. 
+ */
 $(function () {
 
+    var form = layui.form
+    var layer = layui.layer
+    var laypage = layui.laypage
+
+    // 定义一个 美化时间的过滤器
+    template.defaults.imports.dateFormat = function (date) {
+        const dt = new Date(date)
+
+        var y = dt.getFullYear()
+        var m = dt.getMonth() + 1
+        var d = dt.getDate()
+
+        var hh = dt.getHours()
+        var mm = dt.getMinutes()
+        var ss = dt.getSeconds()
+
+        var h = hh < 10 ? '0' + hh : hh
+        var m = mm < 10 ? '0' + mm : mm
+        var s = ss < 10 ? '0' + ss : ss
+
+        return y + '-' + m + '-' + d + '-' + hh + ':' + mm + ':' + ss
+
+
+    }
+
+
+    /**定义一个请求对象 请求对象时会把对象发送到服务器 */
+    var q = {
+        pagenum: 1, // 页码值
+        pagesize: 2, //每一条显示多少数据
+        cate_id: '', // 文章分类的 id
+        state: '', //文章的状态 可选值L:已经发布  在编辑
+
+    }
+    initTable()
+    initCate()
+
+    // 获取文章 列表数据
+    function initTable() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/list',
+            data: q,
+            success: function (res) {
+                if (res.status !== 0) {
+
+                    return layer.msg('访问接口失败 ! ')
+                }
+                console.log(res)
+                //使用 模板 引擎渲染数据
+                layer.msg('访问接口成功 ! ')
+                // 使用 template
+                //TOOD: 使用在后面 先禁用掉 
+                var htmlStr = template('#tpl-table', res)
+                $('tbody').html(htmlStr)
+
+                //页面被渲染完 在渲染页码 根据数量进行渲染
+                renderPage(res.total)
+            }
+        })
+    }
+
+
+    // 初始化 文章分类的 状态
+    function initCate() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                if (res.status !== 0) {
+                    layer.msg('数据 获取失败')
+                }
+                //console.log(res)
+                // 调用模板引擎console.log(res)
+                var htmlStr = template('#tpl-cate', res)
+                console.log(htmlStr)
+                $('[name = cate_id]').html(htmlStr)
+                form.render()
+
+            }
+
+        })
+    }
+
+    //为筛选表单绑定 事件的提交按钮
+    $('#form=search').on('submit', function (e) {
+        //阻止 默认行为
+        e.preventDefault()
+        //获取表单中选中项的值
+        var cate_id = $('[name=cate_id]').val()
+        var state = $('[name=state]').val()
+        //返回值  为查询参数  Q 中 对应参数的属性赋值
+        q.cate_id = cate_id
+        q.state = state
+        //再次 调用 初始化函数
+        initTable()
+    })
+
+
+    //定义 渲染分页的 方法
+    function renderPage(total) {
+        //console.log(total);
+        //调用 layui 方法 
+        layui.use('laypage', function () {
+            var laypage = layui.laypage;
+
+            //执行一个laypage实例
+            laypage.render({
+                elem: 'pageBox', //注意，这里的 test1 是 ID，不用加 # 号
+                count: total, //数据总数，从服务端得到\
+                limit: q.pagesize,  // 每一页显示 几个数据
+                curr: q.pagenum,  // 设置 默认被选中的分页
+                //新增配件
+                layout: ['page', 'next', 'prev'],
+                //重新定义每一页所要展示的值  
+                limits: [2, 3, 5, 10, 20,],
+                //分页 发生切换函数
+                jump: function (obj, first) {
+                    //obj包含了当前分页的所有参数，比如：
+                    // console.log(obj.curr); //得到当前页，以便向服务端请求对应页的数据。
+                    //页码值 反向赋值 给 q 的数组中
+                    q.pagenum = obj.curr
+                    //console.log(obj.limit); //得到每页显示的条数
+                    /**触发 jump 回调的方法
+                     * 1. 点击页码的时候  
+                     * 2. 调用 laypage.render() 方法 就会被触发会陷入循环
+                     */
+
+                    //当页面的 的条目数 分类发生改变后,把新的数据返回到 q 中
+                    q.pagesize = obj.limit
+                    //为了防止自循环  使用 first 判断 first true: 方式2 触发   false L方式1 触发 
+                    if (!first) {
+                        initTable()
+                    }
+                }
+
+            });
+        });
+
+    }
 })
--- assets/js/login.js
+++ assets/js/login.js
@@ -2,23 +2,23 @@
  * @Author: cuibai 2367736060@qq.com
  * @Date: 2022-08-11 21:22:06
  * @LastEditors: cuibai 2367736060@qq.com
- * @LastEditTime: 2022-08-17 20:50:10
- * @FilePath: \web移动端\Node的学习\The_big_event\d01\assets\js\login.js
+ * @LastEditTime: 2022-08-25 19:55:04
+ * @FilePath: \d01\assets\js\login.js
  * @Description: 
  * 
  * Copyright (c) 2022 by cuibai 2367736060@qq.com, All Rights Reserved. 
  */
 //放置入口函数
-$(function() {
+$(function () {
     //点击 注册 链接
-    $('#link_reg').on('click', function(){
+    $('#link_reg').on('click', function () {
         console.log('link_reg click');
         $('.login-box').hide();
         $('.reg-box').show();
     })
 
     // 点击登录 链接
-    $('#link_login').on('click', function(){
+    $('#link_login').on('click', function () {
         $('.login-box').show();
         $('.reg-box').hide();
 
@@ -27,20 +27,20 @@ $(function() {
     // 创建自己的规则
     //从 layui 中获取form 对象  自定义修改
     var form = layui.form
-    var layer  = layui.layer
+    var layer = layui.layer
     //自定义规则,通过form.verify() 函数来修改
     form.verify({
         //规则统一存放
         //定义密码 正则表达  错误提示  
-        pwd: [/^[\S]{6,12}$/,'密码必须6到12位,且不能出现空格'],
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位,且不能出现空格'],
         //新增规则  校验两次 密码输入一致
-        repwd:function(value){
+        repwd: function (value) {
             // 确认 形参拿到密码框内的 内容 
             //难道 密码框的值
             //进行比较判断
             //判断失败 进行 提
             var pwd = $('.reg-box [name=password]').val()
-            if(pwd !== value){
+            if (pwd !== value) {
                 alert('两次密码不一致!')
             }
         }
@@ -48,18 +48,18 @@ $(function() {
 
 
     //监听注册表单 
-    $('#form_reg').on('submit',function(e){
+    $('#form_reg').on('submit', function (e) {
         //组织默认行为
         e.preventDefault();
         // 发起 ajax 请求 post
         $.post(
             '/api/reguser',
-            {username:$('#form_reg [name=username]').val(),password:$('#form_reg [name=password]').val()},
-            function(res){
-                if(res.status !== 0){
-                    layer.msg(res.message); 
+            { username: $('#form_reg [name=username]').val(), password: $('#form_reg [name=password]').val() },
+            function (res) {
+                if (res.status !== 0) {
+                    layer.msg(res.message);
                 }
-                layer.msg('注册成功 即将转到登录'); 
+                layer.msg('注册成功 即将转到登录');
                 console.log(res)
                 // 添加自动跳转 功能
                 $('#link_login').click()
@@ -67,27 +67,27 @@ $(function() {
         )
     })
     // 监听 登录 表单的提交事件
-    $('#form_login').submit(function(e){
+    $('#form_login').submit(function (e) {
         //阻止 默认行为
         e.preventDefault()
         // 发起 ajax 请求 post
         $.ajax({
-            url:'/api/login',
-            method:"post",
+            url: '/api/login',
+            method: "post",
             // 一次性获取表单的全部元素
-            data:$(this).serialize(),
-            success:function(res){
-                if(res.status!== 0){
-                    layer.msg(res.message); 
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    layer.msg(res.message);
                 }
                 layer.msg('注册成功 即将跳转')
                 console.log(res)
                 // 保存 返回的 token值 在locationStorage 中
-                localStorage.setItem('token',res.token)
-                location.href = './index.html'
+                localStorage.setItem('token', res.token)
+                location.href = '/index.html'
             }
 
-            
+
         })
     })
 
